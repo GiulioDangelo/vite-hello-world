7f1a1dc9aa424b135e342a2a67d7d2ed22fde168
GiulioDangelo <dangelogiulio48@gmail.com>
2023-05-17 17:13:04 +0200

    add bonus

---
+++ src/components/AppHero.vue
@@ -0,0 +1,11 @@
+<script></script>
+
+<template>
+    <img src="https://fs-prod-cdn.nintendo-europe.com/media/images/10_share_images/games_15/wiiu_download_software_5/H2x1_WiiUDS_HollowKnight.jpg" alt="">
+</template>
+
+<style scoped>
+    img{
+        width: 1000px;
+    }
+</style>
